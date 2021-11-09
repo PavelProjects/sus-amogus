03e708249e8186751dce266fe395bb25a89bc6b0
John Doe <paveloalexeev52@gmail.com>
2021-11-10 00:34:43 +0300

    finished color picker

--- ImgToSus.py
+++ ImgToSus.py
@@ -58,35 +58,25 @@ class ImgToSus:
         self.cell_w = cell_w
         self.cell_h = cell_h
 
-        if self.debug:
-            self.__show_colors()
-
-    # для дебага, выводит амогусов
-    def __show_colors(self):
-        print(self.colors_keys)
-        for key in self.colors_img.keys():
-            try:               
-                for frame in self.colors_img[key]:
-                    cv2.imshow("frame", frame)
-                    cv2.waitKey()
-            except Exception as e:
-                print(str(e))
-
-    # Вернет название цвета исходя из rgb кода
-    def __closest_colour(self, requested_colour):
+    # Вернет подходящий sus цвет
+    def __get_sus_color(self, requested_colour):
         min_colours = {}
-        for key, name in webcolors.CSS21_HEX_TO_NAMES.items():
-            r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        for b_c, g_c, r_c in self.colors_keys.keys():
             rd = (r_c - requested_colour[0]) ** 2
             gd = (g_c - requested_colour[1]) ** 2
             bd = (b_c - requested_colour[2]) ** 2
-            min_colours[(rd + gd + bd)] = name
+            min_colours[(rd + gd + bd)] = self.colors_keys[(b_c, g_c, r_c)]
         return min_colours[min(min_colours.keys())]
 
+    #TODO
     # Поиск цвета для замены клетки 
     # Возвращает bgr ключ цвета (например (197, 17, 17))
     def __get_cell_color(self, frame):
-        return (17, 17, 197)
+        h, w, _ = frame.shape
+        # if h <= 2 or w <= 2:
+        #     return (0,0,0)
+        print((h, w))
+        return frame[h//2, w//2]
 
     # Загрузка основного изображения для преобразования
     def load_img(self, path: str):
@@ -105,23 +95,21 @@ class ImgToSus:
             raise Exception("NO IMAGE LOADED")
 
         height, width, _ = self.img.shape
-        for x in range(0, width, self.cell_w):
-            for y in range(0, height, self.cell_h):
-                color = self.__get_cell_color(self.img[x:x + self.cell_h, y:y + self.cell_w])
-                color_key = self.colors_keys[color]
+        for y in range(0, height, self.cell_h):
+            for x in range(0, width, self.cell_w):
+                print((y, y + self.cell_w))
+                print((height, width))
+                color = self.__get_cell_color(self.img[y:y + self.cell_h, x:x + self.cell_w])
+                color_key = self.__get_sus_color(color)
+                print((color, color_key))
                 if color_key != None:
                     color_img = self.colors_img[color_key]
-                    try:
-                        self.img[y:y + self.cell_h, x:x + self.cell_w, :3] = color_img[2]
-                    except Exception as e:
-                        print(f"#{y} - {x} | {self.img[y:y + self.cell_h, x:x + self.cell_w, :3].shape} {str(e)}")
+                    self.img[y:y + self.cell_h, x:x + self.cell_w, :3] = color_img[2]
 
-        cv2.imshow("e", self.img)
-        cv2.waitKey()
+        # cv2.imshow("e", self.img)
+        # cv2.waitKey()
     
     # сохранение изменненого изображения
     def save_converted_image(self):
-        if self.result.all() == None:
-            self.convert_img()
         cv2.imwrite(CONVERTED_PATH, self.img)
 
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from ImgToSus import ImgToSus
 
-conv = ImgToSus(debug=False)
-conv.load_img("./static/pupa.jpg")
+conv = ImgToSus(debug=False, scale=6)
+conv.load_img("./static/test.jpg")
 conv.convert_img()
+conv.save_converted_image()
 input("loaded")
